5f5cf01d9fd7ca5b29d313fdfabc1abde0d2a255
jk-jumur <developer.jumur@gmail.com>
2026-04-30 22:42:55 +0600

    explore api and JSON

---
+++ script.js
@@ -0,0 +1,28 @@
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      
+
+
+
+
+// console.log("Explore API");
+
+// const person = {
+//       name: "JK",
+//       fruit: "avocado",
+//       dish: "chaomin",
+//       friends: ["alim", "kalim", "lamim"],
+//       isRich: true,
+// };
+// console.log(person);
+// // JSON -> JS object with Notation
+// //JSON.stringify -> JSON
+// //JSON perse -> object 
+
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const perseJSON = JSON.parse(personJSON);
+// console.log(perseJSON);
