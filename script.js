e4fe6255aa86995c908fe8ac07dd873259d4ed89
jk-jumur <developer.jumur@gmail.com>
2026-05-01 01:48:32 +0600

    how to work posts

--- script.js
+++ script.js
@@ -9,9 +9,36 @@
       
 const loadData = () => {
        fetch("https://jsonplaceholder.typicode.com/todos/1")
+        //promise of response
       .then((response) => response.json())
+      //promise of json data
       .then((data) => console.log(data));
 };
+
+
+const loadPost=()=>{
+       const url = "https://jsonplaceholder.typicode.com/posts";
+
+       fetch(url)
+       .then((res)=>res.json())
+       .then((json) => {
+            console.log(json);
+            displayPost(json);
+       });
+};
+
+
+const displayPost =(posts)=>{
+      //  console.log(posts);
+       posts.forEach(post=>{
+           console.log(post);  
+       })
+
+            
+
+            
+       
+}
 
 
 
